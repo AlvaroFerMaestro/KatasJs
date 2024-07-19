bf3991dff5f5b4b4f858400d83dd8b2ba77b52ff
AlvaroFerMaestro <141323952+AlvaroFerMaestro@users.noreply.github.com>
2024-07-19 13:14:25 +0200

    Add files via upload

---
+++ EJERCICIOS JS/ejercicio-1/index.js
@@ -0,0 +1,14 @@
+/* 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y' */
+
+const myFavoriteHero = "hulk "
+
+var x = 50;
+var h = 5;
+var y = 10
+
+var z = h + y;
+
+console.log(z);
